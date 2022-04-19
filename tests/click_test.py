b8139047f7ea9b19fc75ad61e51ea169e42460b2
pavly <pr253@njit.edu>
2022-04-19 11:36:41 -0400

    anew.6

--- tests/click_test.py
+++ tests/click_test.py
@@ -12,9 +12,9 @@ def test_create_log_folder():
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
     # set the name of the apps log folder to logs
-    logdir = os.path.join(root, '../logs')
+    logdir = os.path.join(root, '../app/logs')
     # make a directory if it doesn't exist
-    assert os.path.exists(logdir) == True
+    assert os.path.exists(logdir) is True
 
 def test_create_database():
     response = runner.invoke(create_database)
@@ -23,4 +23,4 @@ def test_create_database():
     # set the name of the apps log folder to logs
     dbdir = os.path.join(root, '../database')
     # make a directory if it doesn't exist
-    assert os.path.exists(dbdir) == True
+    assert os.path.exists(dbdir) is True
